ecf0630479c9f31ca5684130ccb84def906a40a0
Parin Gandhi <Parin Gandhi>
2016-07-18 21:26:43 -0400

    Added welcome page

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,37 +3,10 @@
 
   angular
     .module('paringandhiInfo')
-    .controller('MainController', MainController);
-
-  /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+    .controller('MainController', function ($timeout, webDevTec, toastr, $rootScope) {
     var vm = this;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1465267061638;
-    vm.showToastr = showToastr;
+      $rootScope.projectName = "mainPage";
 
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-  }
+  });
 })();
