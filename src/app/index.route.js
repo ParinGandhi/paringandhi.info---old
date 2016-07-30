518aa2c561eb2527a1fba6429486d4b66cb7c0fc
Parin Gandhi <Parin Gandhi>
2016-07-30 14:12:43 -0400

    Formatted files

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,45 +1,45 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('paringandhiInfo')
-    .config(routeConfig);
+    angular
+        .module('paringandhiInfo')
+        .config(routeConfig);
 
-  function routeConfig($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'vm'
-      })
-    .when('/quotes', {
-        templateUrl: 'app/quotes/quotes.html',
-        controller: 'QuotesController',
-        controllerAs: 'vm'
-      })
-    .when('/beer', {
-        templateUrl: 'app/beer/beer.html',
-        controller: 'BeerController',
-        controllerAs: 'vm'
-      })
-    .when('/movies', {
-        templateUrl: 'app/movies/movies.html',
-        controller: 'MoviesController',
-        controllerAs: 'vm'
-      })
-    .when('/mortgage', {
-        templateUrl: 'app/mortgage/mortgage.html',
-        controller: 'MortgageController',
-        controllerAs: 'vm'
-      })
-    .when('/apod', {
-        templateUrl: 'app/apod/apod.html',
-        controller: 'APODController',
-        controllerAs: 'vm'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }
+    function routeConfig($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/main/main.html',
+                controller: 'MainController',
+                controllerAs: 'vm'
+            })
+            .when('/quotes', {
+                templateUrl: 'app/quotes/quotes.html',
+                controller: 'QuotesController',
+                controllerAs: 'vm'
+            })
+            .when('/beer', {
+                templateUrl: 'app/beer/beer.html',
+                controller: 'BeerController',
+                controllerAs: 'vm'
+            })
+            .when('/movies', {
+                templateUrl: 'app/movies/movies.html',
+                controller: 'MoviesController',
+                controllerAs: 'vm'
+            })
+            .when('/mortgage', {
+                templateUrl: 'app/mortgage/mortgage.html',
+                controller: 'MortgageController',
+                controllerAs: 'vm'
+            })
+            .when('/apod', {
+                templateUrl: 'app/apod/apod.html',
+                controller: 'APODController',
+                controllerAs: 'vm'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
 
 })();
